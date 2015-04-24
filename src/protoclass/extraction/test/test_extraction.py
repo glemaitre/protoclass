2a3989415f5c9a21a3da86c23b4e15e399c3950c
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-04-24 16:33:10 +0200

    Wrote the test to save all 2D maps

--- src/protoclass/extraction/test/test_extraction.py
+++ src/protoclass/extraction/test/test_extraction.py
@@ -8,24 +8,67 @@
 #==============================================================================
 
 # Import the needed libraries
+# Matplotlib library
 import matplotlib.pyplot as plt
+# Numpy libreary
 import numpy as np
+# OS library
+import os
+from os.path import join
 
+# Our module to read DCM volume from a serie of DCM
 from protoclass.tool.dicom_manip import OpenOneSerieDCM
+# Our module to extract Haralick feature maps
 from protoclass.extraction.texture_analysis import HaralickMapExtraction
 
 # Give the path to a patient
-path_to_t2 = '/work/le2i/gu5306le/experiments/Patient 383/T2W'
-# Read a volume
-volume = OpenOneSerieDCM(path_to_t2)
-# Select a slice
-im_2d = volume[:, :, 35]
-# Define the parameters for glcm
-### Window size
-tp_win_size = (9,9)
-### Number of gray levels
-tp_n_gray_levels = 8
-### Compute the Haralick maps
-maps = HaralickMapExtraction(im_2d, win_size=tp_win_size, n_gray_levels=tp_n_gray_levels)
-# Save the maps
-np.save('../data/maps.npy', maps)
+#path_to_t2 = '/work/le2i/gu5306le/experiments/Patient 383/T2W'
+path_to_data = '/work/le2i/gu5306le/experiments'
+path_to_data = '/home/lemaitre/Documents/Data/experiments'
+path_t2w = 'T2W'
+path_haralick = 'haralick'
+
+# Go through all the patients directories
+for dirs in os.listdir(path_to_data):
+    path_patient = join(path_to_data, dirs)
+    path_dcm = join(path_patient, path_t2w)
+    print 'Reading data from the directory {}'.format(path_dcm)
+
+    # Read a volume
+    volume = OpenOneSerieDCM(path_dcm)
+
+    #################################################################################
+    ### 2D volume
+
+    # Define the parameters for glcm
+    ### Window size
+    tp_win_size = (9,9)
+    ### Number of gray levels
+    tp_n_gray_levels = 64
+
+    # Create the ouput list
+    patient_maps = []
+
+    # Go through each slice of the volume
+    for sl in range(volume.shape[2]):
+        ### Compute the Haralick maps
+        patient_maps.append(HaralickMapExtraction(volume[:, :, sl], win_size=tp_win_size, n_gray_levels=tp_n_gray_levels))
+        
+    # Create a directory if not existing
+    path_haralick_saving = join(path_patient, path_haralick)
+    if not os.path.exists(path_haralick_saving):
+        os.makedirs(path_haralick_saving)
+
+    # Convert the maps list to array
+    patient_maps = np.array(patient_maps)
+
+    # Roll the first axis to obtain [orientation][stats][y,x,z]
+    patient_maps = np.rollaxis(patient_maps, 0, 5)
+
+    # Save a volume for each orientation and stats
+    n_orientations = 4
+    n_statistics = 13
+    for o, s in zip (range(n_orientations), range(n_statistics)):
+        # Save the current volume
+        filename = join(path_haralick_saving, 'volume_' + str(o) + '_' + str(s) + '.npy')
+        np.save(filename, patient_maps[o][s][:, :, :])
