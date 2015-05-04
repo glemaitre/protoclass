307307c3c8bbeb32009142e67a028ca2988738ad
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-05-04 19:17:27 +0200

    Comment properly the normalisation module

--- src/protoclass/preprocessing/normalisation.py
+++ src/protoclass/preprocessing/normalisation.py
@@ -34,34 +34,95 @@ class GaussianNormalisation(object):
     """
 
     def __init__(self, norm_factor=1., mu=0., sigma=1.):
+        """Constructor of the GaussianNormalisation object
+        
+        Parameters
+        ----------
+        norm_factor: double
+            Amplitude of the Gaussian representation
+        mu: double
+            Mean of the Gaussian representation
+        sigma: double
+            Standard deviation of the Gaussian representation
+    
+        """
 
         self.__norm_factor = norm_factor
         self.__mu = mu
         self.__sigma = sigma
 
     def __InitFromData__(self, x, norm_factor):
-        # TODO: put more information about the definition
-        # GOAL: Initialisation of the mean and std using the data
+        """Function to initialise the class members using data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data from which the mean and
+            standard deviation are derived
+        norm_factor: double
+            Amplitude of the Gaussian representation
+
+        """
 
         self.__norm_factor = norm_factor
         self.__mu = np.mean(x)
         self.__sigma = np.std(x)
 
     def GetParameters(self):
-        # TODO: put more information about the definition
-        # GOAL: Get the parameters
-
+        """Function to get the parameter of the object
+        
+        Returns
+        -------
+        params: tuple 
+            Return a tuple with the amplitude, mean and std of the 
+            Gaussian representation
+    
+        """
+        
         return (self.__norm_factor, self.__mu, self.__sigma)
 
     def __Parametrization__(self, x, norm_factor, mu, sigma):
-        # TODO: put more information about the definition
-        # GOAL: Gaussian fitting pdf
+        """Function to parametrise the Gaussian PDF
+        
+        Parameters
+        ----------
+        x : 1d-array
+            Array containing the x values in order to compute the PDF
+        norm_factor: double
+            Amplitude of the Gaussian representation
+        mu: double
+            Mean of the Gaussian representation
+        sigma: double
+            Standard deviation of the Gaussian representation
+
+        Returns
+        -------
+        pdf: 1d-array 
+            Return the Gaussian PDF according the parameters
+    
+        """
         
         return norm.pdf(x, mu, sigma) / norm_factor
 
     def __ComputeHistogram__(self, x, x_range):
-        # TODO: put more information about the definition
-        # GOAL: compute this histogram of x taking care about the data range
+        """Function to compute the histogram of the data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data
+        x_range: tuple
+            Tuple containing the minimum and maxumum to consider
+            to build the histogram of the data x
+
+        Returns
+        -------
+        pdf: 1d-array 
+            Return the PDF of x using the range provided by the user
+        bin_edge: 1d-array
+            Return the bins corresponding to the PDF
+    
+        """
         
         # Compute the histogram for the data x with unit bins
         pdf_rel, bin_edges_rel = np.histogram(x, bins=(np.max(x) - np.min(x)), density=True)
@@ -77,8 +138,24 @@ class GaussianNormalisation(object):
         return (pdf_abs, bin_edges_abs)
 
     def Fit(self, x, x_range):
-        # TODO: put more information about the definition
-        # GOAL: fit the pdf of the data x. We need to compute the histogram first
+        """Function to find the best member parameters to fit 
+           the class parametrisation using Levenverg-Marquardt
+           curve fitting
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data
+        x_range: tuple
+            Tuple containing the minimum and maxumum to consider
+            to build the histogram of the data x
+
+        Returns
+        -------
+        error_fit: double
+            Return the fitting error
+    
+        """
 
         # Check that the value in x_range make sense
         if not ((x_range[0] <= np.min(x))&(x_range[1] >= np.max(x))):
@@ -113,14 +190,36 @@ class GaussianNormalisation(object):
         return pcov
 
     def Normalise(self, x):
-        # TODO: put more information about the definition
-        # GOAL: Normalise the data x.
+        """Function to normalise the data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the unormalised data
+
+        Returns
+        -------
+        x_normalised: ndarray 
+            Return the data x normalised
+    
+        """
 
         return (x - (self.__mu * self.__norm_factor)) / (self.__sigma * self.__norm_factor)
 
     def Denormalise(self, x):
-        # TODO: put more information about the definition
-        # GOAL: Denormalise the data x.
+        """Function to unormalise the data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the normalised data
+
+        Returns
+        -------
+        x_unormalised: ndarray 
+            Return the data x unormalised
+    
+        """
 
         return x * (self.__sigma * self.__norm_factor) + (self._mu * self.__norm_factor)
 
@@ -132,6 +231,20 @@ class RicianNormalisation(object):
     """
 
     def __init__(self, norm_factor=1., v=0., loc=0., sigma=1.):
+        """Constructor of the GaussianNormalisation object
+        
+        Parameters
+        ----------
+        norm_factor: double
+            Amplitude of the Rician representation
+        v: double
+            Distance to the center of the bivariate distribution
+        loc: double
+            Initial shift of the Rician representation
+        sigma: double
+            Scale of the Rician representation
+    
+        """
 
         self.__norm_factor = norm_factor
         self.__v = v
@@ -139,8 +252,17 @@ class RicianNormalisation(object):
         self.__sigma = sigma
 
     def __InitFromData__(self, x, norm_factor):
-        # TODO: put more information about the definition
-        # GOAL: Initialisation of the mean and std using the data
+        """Function to initialise the class members using data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data from which the mean and
+            standard deviation are derived
+        norm_factor: double
+            Amplitude of the Rician representation
+
+        """
         
         self.__norm_factor = norm_factor
         self.__v = np.mean(x)
@@ -148,21 +270,61 @@ class RicianNormalisation(object):
         self.__sigma = np.std(x)
 
     def GetParameters(self):
-        # TODO: put more information about the definition
-        # GOAL: Get the parameters
+        """Function to get the parameter of the object
+        
+        Returns
+        -------
+        params: tuple 
+            Return a tuple the parameters of the Rician representation
+    
+        """
 
         return (self.__norm_factor, self.__v, self.__loc, self.__sigma)
 
     def __Parametrization__(self, x, norm_factor, v, loc, sigma):
-        # TODO: put more information about the definition
-        # GOAL: Gaussian fitting pdf
+        """Function to parametrise the Rician PDF
         
+        Parameters
+        ----------
+        x : 1d-array
+            Array containing the x values in order to compute the PDF
+        norm_factor: double
+            Amplitude of the Rician representation
+        v: double
+            Distance to center of the Rician representation
+        loc: double
+            Location of the Rician representation
+        sigma: double
+            Scale of the Rician representation
+
+        Returns
+        -------
+        pdf: 1d-array 
+            Return the Gaussian PDF according the parameters
+    
+        """
+
         return rice.pdf(x, v, loc, sigma) / norm_factor
 
     def __ComputeHistogram__(self, x, x_range):
-        # TODO: put more information about the definition
-        # GOAL: compute this histogram of x taking care about the data range
+        """Function to compute the histogram of the data
         
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data
+        x_range: tuple
+            Tuple containing the minimum and maxumum to consider
+            to build the histogram of the data x
+
+        Returns
+        -------
+        pdf: 1d-array 
+            Return the PDF of x using the range provided by the user
+        bin_edge: 1d-array
+            Return the bins corresponding to the PDF
+    
+        """        
         # Compute the histogram for the data x with unit bins
         pdf_rel, bin_edges_rel = np.histogram(x, bins=(np.max(x) - np.min(x)), density=True)
 
@@ -177,9 +339,24 @@ class RicianNormalisation(object):
         return (pdf_abs, bin_edges_abs)
 
     def Fit(self, x, x_range):
-        # TODO: put more information about the definition
-        # GOAL: fit the pdf of the data x. We need to compute the histogram first
+        """Function to find the best member parameters to fit 
+           the class parametrisation using Levenverg-Marquardt
+           curve fitting
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the data
+        x_range: tuple
+            Tuple containing the minimum and maxumum to consider
+            to build the histogram of the data x
 
+        Returns
+        -------
+        error_fit: double
+            Return the fitting error
+    
+        """
         # Check that the value in x_range make sense
         if not ((x_range[0] <= np.min(x))&(x_range[1] >= np.max(x))):
             raise ValueError('normalisation.GaussianNormalisation: Wrong range specifications for the x.')
@@ -213,13 +390,35 @@ class RicianNormalisation(object):
         return pcov
 
     def Normalise(self, x):
-        # TODO: put more information about the definition
-        # GOAL: Normalise the data x.
+        """Function to normalise the data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the unormalised data
+
+        Returns
+        -------
+        x_normalised: ndarray 
+            Return the data x normalised
+    
+        """
 
         return (x - rice.mean(self.__v, self.__loc, self.__sigma) * self.__norm_factor) / (self.__sigma * self.__norm_factor)
 
     def Denormalise(self, x):
-        # TODO: put more information about the definition
-        # GOAL: Denormalise the data x.
+        """Function to unormalise the data
+        
+        Parameters
+        ----------
+        x: ndarray
+            Array containing the normalised data
+
+        Returns
+        -------
+        x_unormalised: ndarray 
+            Return the data x unormalised
+    
+        """
 
         return x * (self.__sigma * self.__norm_factor) + (rice.mean(self.__v, self.__loc, self.__sigma) * self.__norm_factor)
