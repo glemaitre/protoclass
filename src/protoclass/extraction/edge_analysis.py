dea10126489971c9f17ae36110fb29af67710c84
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-05-05 01:14:10 +0200

    Implement Gabor filer bank

--- src/protoclass/extraction/edge_analysis.py
+++ src/protoclass/extraction/edge_analysis.py
@@ -16,6 +16,9 @@ import scipy as sp
 from scipy.ndimage.filters import generic_gradient_magnitude, generic_laplace
 ### Scipy edge detector
 from scipy.ndimage.filters import prewitt, sobel, laplace
+# Scikit-image library
+### Gabor filtering
+from skimage.filters import gabor_kernel
 # Scikit-learn
 ### Module to extract 2D patches
 from sklearn.feature_extraction import image
@@ -45,9 +48,20 @@ def EdgeMapExtraction(im, **kwargs):
 
     # Assign the edge detector
     edge_detector= kwargs.pop('edge_detector', 'Sobel1stDev')
-    detector_list = ['Sobel1stDev', 'Prewitt1stDev', 'Sobel2ndDev', 'Prewitt2ndDev']
+    detector_list = ['Sobel1stDev', 'Prewitt1stDev', 'Sobel2ndDev', 'Prewitt2ndDev', 'GaborBank']
     if not any(edge_detector in dl for dl in detector_list):
         raise ValueError('mahotas.edge: The name of the detector is unknown.')
+
+    if edge_detector == 'GaborBank':
+        # Extract the value of the parameters
+        n_freq = kwargs.pop('n_freq', 10)
+        freq_range = kwargs.pop('freq_range', (.05, .2))
+        n_theta = kwargs.pop('n_theta', 6)
+        win_size = kwargs.pop('win_size', (15., 15.))
+        # Generate the different kernel which are needed
+        kernels_gabor = GaborKernelBank(n_freq, freq_range, n_theta, win_size)
+
+        return kernels_gabor
 
     if edge_detector == 'Sobel1stDev':
         edge_im = generic_gradient_magnitude(im, sobel)
@@ -59,3 +73,31 @@ def EdgeMapExtraction(im, **kwargs):
         edge_im = generic_laplace(im, prewitt)
 
     return edge_im
+
+########################################################################################
+### 2D implementation
+
+def GaborKernelBank(n_freq=10, freq_range=(.05, .2), n_theta=6, win_size=(15., 15.)):
+    # TODO: write important information
+    # GOAL: return the different kernel to use when performing the gabor filtering 
+
+    # Affect the right values for the sigmas
+    s_y = (win_size[0] - 1.) / 6. 
+    s_x = (win_size[1] - 1.) / 6.
+
+    # Generate the values for the different thetas
+    thetas = np.linspace(0, np.pi, int(n_theta))
+
+    # Generate the values for the different frequencies
+    freqs = np.linspace(freq_range[0], freq_range[1], int(n_freq))
+
+    kernels = []
+    for theta in thetas:
+        for freq in freqs:
+            kernel = np.real(gabor_kernel(freq, 
+                                          theta=theta,
+                                          sigma_x=s_y, 
+                                          sigma_y=s_y))
+            kernels.append(kernel)
+
+    return kernels
