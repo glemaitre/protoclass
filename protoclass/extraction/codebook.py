8e874afc5a2c4ca8e4a32cfef0e817846c14ad9f
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-06-10 12:09:02 +0200

    Add the bof feature

---
+++ protoclass/extraction/codebook.py
@@ -0,0 +1,307 @@
+"""
+The :mod:`coodebook` module includes utilities to create a Bag-of-(visual)Fetures,
+that takes advantage of :mod:`sklearn`.
+"""
+
+# Authors: Joan Massich
+#          Guillaume Lemaitre
+#
+# License: MIT
+
+import numpy as np
+
+from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
+
+# import warnings
+
+# import scipy.sparse as sp
+
+# from sklearn.metrics.pairwise import euclidean_distances
+# from sklearn.utils.extmath import row_norms, squared_norm
+# from sklearn.utils.sparsefuncs_fast import assign_rows_csr
+# from sklearn.utils.sparsefuncs import mean_variance_axis
+# from sklearn.utils.fixes import astype
+# from sklearn.utils import check_array
+# from sklearn.utils import check_random_state
+# from sklearn.utils import as_float_array
+# from sklearn.utils import gen_batches
+# from sklearn.utils.validation import check_is_fitted
+# from sklearn.utils.random import choice
+# from sklearn.externals.joblib import Parallel
+# from sklearn.externals.joblib import delayed
+
+
+class CoodeBook(BaseEstimator, ClusterMixin, TransformerMixin):
+    """Code Book creation and manimpulation for Bag-of-(visual)Fetures.
+
+    Parameters
+    ----------
+
+    n_words : int, optional, default: 36
+        The number of clusters to form as well as the number of
+        words (centroids) to generate.
+
+    cluster_core : sklearn.cluster, default: KMeans
+        Clustering technique used to quantisize the feature space to
+        generate the code book.
+        #TODO: its default should be described by _default_clustering()
+
+    max_iter : int, default: 300
+        Maximum number of iterations of the k-means algorithm for a
+        single run.
+
+    n_init : int, default: 10
+        Number of time the k-means algorithm will be run with different
+        centroid seeds. The final results will be the best output of
+        n_init consecutive runs in terms of inertia.
+
+    init : {'k-means++', 'random' or an ndarray}
+        Method for initialization, defaults to 'k-means++':
+
+        'k-means++' : selects initial cluster centers for k-mean
+        clustering in a smart way to speed up convergence. See section
+        Notes in k_init for more details.
+
+        'random': choose k observations (rows) at random from data for
+        the initial centroids.
+
+        If an ndarray is passed, it should be of shape (n_clusters, n_features)
+        and gives the initial centers.
+
+
+    precompute_distances : {'auto', True, False}
+        Precompute distances (faster but takes more memory).
+
+        'auto' : do not precompute distances if n_samples * n_words > 12
+        million. This corresponds to about 100MB overhead per job using
+        double precision.
+
+        True : always precompute distances
+
+        False : never precompute distances
+
+    tol : float, default: 1e-4
+        Relative tolerance with regards to inertia to declare convergence
+
+    n_jobs : int
+        The number of jobs to use for the computation. This works by computing
+        each of the n_init runs in parallel.
+
+        If -1 all CPUs are used. If 1 is given, no parallel computing code is
+        used at all, which is useful for debugging. For n_jobs below -1,
+        (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
+        are used.
+
+    random_state : integer or numpy.RandomState, optional
+        The generator used to initialize the centers. If an integer is
+        given, it fixes the seed. Defaults to the global numpy random
+        number generator.
+
+    verbose : int, default 0
+        Verbosity mode.
+
+    copy_x : boolean, default True
+        When pre-computing distances it is more numerically accurate to center
+        the data first.  If copy_x is True, then the original data is not
+        modified.  If False, the original data is modified, and put back before
+        the function returns, but small numerical differences may be introduced
+        by subtracting and then adding the data mean.
+
+    Attributes
+    ----------
+    cook_book_ : array, [n_words, n_features]
+        Coordinates of cluster centers
+
+    labels_ :
+        Labels of each point
+
+    inertia_ : float
+        Sum of distances of samples to their closest cluster center.
+
+    Notes
+    ------
+    The k-means problem is solved using Lloyd's algorithm.
+
+    The average complexity is given by O(k n T), were n is the number of
+    samples and T is the number of iteration.
+
+    The worst case complexity is given by O(n^(k+2/p)) with
+    n = n_samples, p = n_features. (D. Arthur and S. Vassilvitskii,
+    'How slow is the k-means method?' SoCG2006)
+
+    In practice, the k-means algorithm is very fast (one of the fastest
+    clustering algorithms available), but it falls in local minima. That's why
+    it can be useful to restart it several times.
+
+    See also
+    --------
+
+    dictionary_code
+    """
+    #TODO: test n_words default = 36
+    #TODO: does make sense these paremters: max_iter, n_init
+    #TODO: change the cluter_core from cluster_code=None to
+    #      cluster_code=_default_cluster(), doing all the apropiated
+    #      changes. Check that BaseEstimator asks for strict declaration
+    #
+    #      def _default_cluster(self, n_words=36,
+    #                   init='k-means++', n_init=10, max_iter=300,
+    #                   tol=1e-4, precompute_distances='auto',
+    #                   verbose=0, random_state=None, copy_x=True, n_jobs=1):
+    #          """Default space clustering strategy to determine the code book"""
+    #          from sklearn.cluster import KMeans
+    #          return KMeans(n_clusters=n_words, ...)
+    #
+    #       Then self.set_param can also be used to setup the parameters for the
+    #       current classification methodology
+
+    def __init__(self, n_words=36, cluster_core=None, init='k-means++',
+                 n_init=10, max_iter=300, tol=1e-4, precompute_distances='auto',
+                 verbose=0, random_state=None, copy_x=True, n_jobs=1):
+
+        if hasattr(init, '__array__'):
+            n_words = init.shape[0]
+            init = np.asarray(init, dtype=np.float64)
+
+        self.n_words = n_words
+        self.cluster_core = cluster_core
+        self.init = init
+        self.max_iter = max_iter
+        self.tol = tol
+        self.precompute_distances = precompute_distances
+        self.n_init = n_init
+        self.verbose = verbose
+        self.random_state = random_state
+        self.copy_x = copy_x
+        self.n_jobs = n_jobs
+
+        if self.cluster_core is None:
+            from sklearn.cluster import KMeans
+            self.cluster_core = KMeans(n_clusters=self.n_words, init=init, max_iter=max_iter,
+                                       tol=tol, precompute_distances=precompute_distances,
+                                       n_init=n_init, verbose=verbose, random_state=random_state,
+                                       copy_x=copy_x, n_jobs=n_jobs)
+
+    def _check_fit_data(self, X):
+        """Verify that the number of samples given is larger than n_words"""
+        X = check_array(X, accept_sparse='csr', dtype=np.float64)
+        if X.shape[0] < self.n_words:
+            raise ValueError("n_samples=%d should be >= n_words=%d" % (
+                X.shape[0], self.n_words))
+        return X
+
+    def _check_test_data(self, X):
+        X = check_array(X, accept_sparse='csr')
+        n_samples, n_features = X.shape
+        expected_n_features = self.cook_book_.shape[1]
+        if not n_features == expected_n_features:
+            raise ValueError("Incorrect number of features. "
+                             "Got %d features, expected %d" % (
+                                 n_features, expected_n_features))
+        if X.dtype.kind != 'f':
+            warnings.warn("Got data type %s, converted to float "
+                          "to avoid overflows" % X.dtype,
+                          RuntimeWarning, stacklevel=2)
+            X = X.astype(np.float)
+
+        return X
+
+    def fit(self, X, y=None):
+        """Compute the clustering of the space.
+        #TODO: right now only for K_means, however a dispatcher is
+               needed so that other clustering stragegies are called
+               indisticntly
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix, shape=(n_samples, n_features)
+        """
+        self.cluster_core = self.cluster_core.fit(X, y)
+        return self
+
+    def fit_predict(self, X, y=None):
+        """Compute cluster centers and predict cluster index for each sample.
+
+        Convenience method; equivalent to calling fit(X) followed by
+        predict(X).
+        """
+        #return self.fit(X).labels_
+        raise NotImplementedError
+
+    def fit_transform(self, X, y=None):
+        """Compute clustering and transform X to cluster-distance space.
+
+        Equivalent to fit(X).transform(X), but more efficiently implemented.
+        """
+        # Currently, this just skips a copy of the data if it is not in
+        # np.array or CSR format already.
+        # XXX This skips _check_test_data, which may change the dtype;
+        # we should refactor the input validation.
+        # 
+        # X = self._check_fit_data(X)
+        # return self.fit(X)._transform(X)
+        raise NotImplementedError
+
+    def transform(self, X, y=None):
+        """Transform X to a cluster-distance space.
+
+        In the new space, each dimension is the distance to the cluster
+        centers.  Note that even if X is sparse, the array returned by
+        `transform` will typically be dense.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to transform.
+
+        Returns
+        -------
+        X_new : array, shape [n_samples, k]
+            X transformed in the new space.
+        """
+        # check_is_fitted(self, 'cook_book_')
+
+        # X = self._check_test_data(X)
+        # return self._transform(X)
+        raise NotImplementedError
+
+    def _transform(self, X):
+        """guts of transform method; no input validation"""
+        # return euclidean_distances(X, self.cook_book_)
+        raise NotImplementedError
+
+
+    def predict(self, X):
+        """Predicts the index value of the closest word within the code book.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to predict.
+
+        Returns
+        -------
+        labels : array, shape [n_samples,]
+            Index of the closest word within the code book.
+        """
+        return self.cluster_core.predict(X)
+
+    def get_dictionary(self):
+        """Retrieves the words forming the code book
+
+        Returns
+        -------
+        dictionary : array, shape [n_words, n_features]
+            Code book elements (words of the dictionary) represented
+            in the feature space
+        """
+        #TODO: check that the coodebook is fitted
+        return self.cluster_core.cluster_centers_
+
+    def get_BoF_descriptor(self,X):
+
+        # norm = lambda x: x.astype(float)/np.linalg.norm(x)
+        # return norm(np.bincount(self.predict(X)))
+        return np.histogram(self.predict(X),
+                            bins=range(self.n_words+1),
+                            density=True)
---
+++ protoclass/extraction/test/test_codebook.py
@@ -0,0 +1,198 @@
+"""Testing for Code Book"""
+
+# Get the correct unittest library
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from codebook import *
+
+## -----
+import sys
+
+import numpy as np
+
+from sklearn.datasets.samples_generator import make_blobs
+
+from sklearn.utils.testing import assert_equal
+# from sklearn.utils.testing import assert_array_equal
+# from sklearn.utils.testing import assert_array_almost_equal
+# from sklearn.utils.testing import SkipTest
+from sklearn.utils.testing import assert_almost_equal
+from sklearn.utils.testing import assert_raises
+# from sklearn.utils.testing import assert_raises_regexp
+# from sklearn.utils.testing import assert_true
+from sklearn.utils.testing import assert_greater
+from sklearn.utils.testing import assert_less
+# from sklearn.utils.testing import assert_warns
+# from sklearn.utils.testing import if_not_mac_os
+
+# from sklearn.utils.extmath import row_norms
+from sklearn.metrics.cluster import v_measure_score
+from sklearn.cluster import KMeans, k_means
+# from sklearn.cluster import MiniBatchKMeans
+# from sklearn.cluster.k_means_ import _labels_inertia
+# from sklearn.cluster.k_means_ import _mini_batch_step
+# from sklearn.externals.six.moves import cStringIO as StringIO
+
+
+class Test_data_convert(unittest.TestCase):
+
+    # def _check_fitted_model(self):
+    def test_fitted_model(self):
+
+        # non centered, sparse centers to check the
+        centers = np.array([
+            [0.0, 5.0, 0.0, 0.0, 0.0],
+            [1.0, 1.0, 4.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0, 5.0, 1.0],
+            ])
+        n_samples = 100
+        n_clusters, n_features = centers.shape
+        X, true_labels = make_blobs(n_samples=n_samples, centers=centers,
+                                    cluster_std=1., random_state=42)
+
+        cbook = CoodeBook(n_words=3)
+        cbook = cbook.fit(X) # TODO: Is it neaded to reasign? or it can be just cbook.fit(X)
+
+        # check that the number of clusters centers and distinct labels match
+        # the expectation
+        centers = cbook.get_dictionary()
+        assert_equal(centers.shape, (n_clusters, n_features))
+
+        labels = cbook.predict(X)
+        assert_equal(np.unique(labels).shape[0], n_clusters)
+
+        # check that the labels assignment are perfect (up to a permutation)
+        assert_equal(v_measure_score(true_labels, labels), 1.0)
+        assert_greater(cbook.cluster_core.inertia_, 0.0)
+
+        # check that the descriptor looks like the homogenous PDF used
+        # to create the original samples
+        cbook_hist = cbook.get_BoF_descriptor(X)
+        expected_value = float(1)/cbook.n_words
+        for bin_value in cbook_hist[0]:
+            assert_less(round(bin_value-expected_value,3), 0.01)
+        #TODO: recode this as assert_almost_equal but with higher tolerance
+        #      since assert_almost_equal is assert(round(a-b,7)==0)
+
+    # def test_kmeans_dtype():
+    #     rnd = np.random.RandomState(0)
+    #     X = rnd.normal(size=(40, 2))
+    #     X = (X * 10).astype(np.uint8)
+    #     km = KMeans(n_init=1).fit(X)
+    #     pred_x = assert_warns(RuntimeWarning, km.predict, X)
+    #     assert_array_equal(km.labels_, pred_x)
+
+    # @if_not_mac_os()
+    # def test_k_means_plus_plus_init_2_jobs():
+    #     if _has_blas_lib('openblas'):
+    #         raise SkipTest('Multi-process bug with OpenBLAS (see issue #636)')
+
+    #     km = KMeans(init="k-means++", n_clusters=n_clusters, n_jobs=2,
+    #                 random_state=42).fit(X)
+    #     _check_fitted_model(km)
+
+    # def test_predict():
+    #     km = KMeans(n_clusters=n_clusters, random_state=42)
+
+    #     km.fit(X)
+
+    #     # sanity check: predict centroid labels
+    #     pred = km.predict(km.cluster_centers_)
+    #     assert_array_equal(pred, np.arange(n_clusters))
+
+    #     # sanity check: re-predict labeling for training set samples
+    #     pred = km.predict(X)
+    #     assert_array_equal(pred, km.labels_)
+
+    #     # re-predict labels for training set using fit_predict
+    #     pred = km.fit_predict(X)
+    #     assert_array_equal(pred, km.labels_)
+
+
+    # def test_score():
+    #     km1 = KMeans(n_clusters=n_clusters, max_iter=1, random_state=42)
+    #     s1 = km1.fit(X).score(X)
+    #     km2 = KMeans(n_clusters=n_clusters, max_iter=10, random_state=42)
+    #     s2 = km2.fit(X).score(X)
+    #     assert_greater(s2, s1)
+
+    # def test_input_dtypes():
+    #     X_list = [[0, 0], [10, 10], [12, 9], [-1, 1], [2, 0], [8, 10]]
+    #     X_int = np.array(X_list, dtype=np.int32)
+    #     X_int_csr = sp.csr_matrix(X_int)
+    #     init_int = X_int[:2]
+
+    #     fitted_models = [
+    #         KMeans(n_clusters=2).fit(X_list),
+    #         KMeans(n_clusters=2).fit(X_int),
+    #         KMeans(n_clusters=2, init=init_int, n_init=1).fit(X_list),
+    #         KMeans(n_clusters=2, init=init_int, n_init=1).fit(X_int),
+    #         # mini batch kmeans is very unstable on such a small dataset hence
+    #         # we use many inits
+    #         MiniBatchKMeans(n_clusters=2, n_init=10, batch_size=2).fit(X_list),
+    #         MiniBatchKMeans(n_clusters=2, n_init=10, batch_size=2).fit(X_int),
+    #         MiniBatchKMeans(n_clusters=2, n_init=10, batch_size=2).fit(X_int_csr),
+    #         MiniBatchKMeans(n_clusters=2, batch_size=2,
+    #                         init=init_int, n_init=1).fit(X_list),
+    #         MiniBatchKMeans(n_clusters=2, batch_size=2,
+    #                         init=init_int, n_init=1).fit(X_int),
+    #         MiniBatchKMeans(n_clusters=2, batch_size=2,
+    #                         init=init_int, n_init=1).fit(X_int_csr),
+    #     ]
+    #     expected_labels = [0, 1, 1, 0, 0, 1]
+    #     scores = np.array([v_measure_score(expected_labels, km.labels_)
+    #                     for km in fitted_models])
+    #     assert_array_equal(scores, np.ones(scores.shape[0]))
+
+
+    # def test_transform():
+    #     km = KMeans(n_clusters=n_clusters)
+    #     km.fit(X)
+    #     X_new = km.transform(km.cluster_centers_)
+
+    #     for c in range(n_clusters):
+    #         assert_equal(X_new[c, c], 0)
+    #         for c2 in range(n_clusters):
+    #             if c != c2:
+    #                 assert_greater(X_new[c, c2], 0)
+
+
+    # def test_fit_transform():
+    #     X1 = KMeans(n_clusters=3, random_state=51).fit(X).transform(X)
+    #     X2 = KMeans(n_clusters=3, random_state=51).fit_transform(X)
+    #     assert_array_equal(X1, X2)
+
+    # def test_k_means_function():
+    #     # test calling the k_means function directly
+    #     # catch output
+    #     old_stdout = sys.stdout
+    #     sys.stdout = StringIO()
+    #     try:
+    #         cluster_centers, labels, inertia = k_means(X, n_clusters=n_clusters,
+    #                                                    verbose=True)
+    #     finally:
+    #         sys.stdout = old_stdout
+    #     centers = cluster_centers
+    #     assert_equal(centers.shape, (n_clusters, n_features))
+
+    #     labels = labels
+    #     assert_equal(np.unique(labels).shape[0], n_clusters)
+
+    #     # check that the labels assignment are perfect (up to a permutation)
+    #     assert_equal(v_measure_score(true_labels, labels), 1.0)
+    #     assert_greater(inertia, 0.0)
+
+    #     # check warning when centers are passed
+    #     assert_warns(RuntimeWarning, k_means, X, n_clusters=n_clusters,
+    #                  init=centers)
+
+    #     # to many clusters desired
+    #     assert_raises(ValueError, k_means, X, n_clusters=X.shape[0] + 1)
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
