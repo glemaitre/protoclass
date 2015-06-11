304833e4a7298223b8f539dfe4f10929db1051cb
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-06-11 10:36:10 +0200

    Add some classification for lbp simple

--- protoclass/pipeline/feature-classification/classification_lbp.py
+++ protoclass/pipeline/feature-classification/classification_lbp.py
@@ -31,7 +31,7 @@ gt = gt_csv.values
 data_filename = gt[:, 0]
 
 # Get the good extension
-radius = 3
+radius = 1
 data_filename = np.array([f + '_nlm_lbp_' + str(radius) + '_hist.npz' for f in data_filename])
 
 label = gt[:, 1]
@@ -52,7 +52,7 @@ else:
     data_folder = '/work/le2i/gu5306le/OCT/lbp_r_' + str(radius) + '_hist_data_npz'
     codebook_filename = '/work/le2i/gu5306le/OCT/lbp_r_' + str(radius) + '_hist_codebook/codebook.pkl'
 
-    get_lbp_data = lambda f: np.load(join(data_folder, f))['vol_lbp_hist']
+    get_lbp_data = lambda f: np.load(join(data_folder, f))['vol_lbp_hist'].reshape(-1)
     from sklearn.externals import joblib
 
     codebook_list = joblib.load(codebook_filename)
@@ -67,16 +67,15 @@ else:
         results_by_codebook = []
         for current_cbook in codebook_list[idx_test]:
             # Collect the current training data
-            training_normal = [current_cbook.get_BoF_descriptor(get_lbp_data(f))[0] for f in pat_train_norm]
-            training_dme = [current_cbook.get_BoF_descriptor(get_lbp_data(f))[0] for f in pat_train_dme]
+            training_normal = [get_lbp_data(f) for f in pat_train_norm]
+            training_dme = [get_lbp_data(f) for f in pat_train_dme]
 
             # Compose the training ( data & labels )
             training_data = np.array(training_normal+training_dme)
             training_label = np.array([0]*len(training_normal) + [1]*len(training_dme), dtype=int)
 
             # Compose the testing
-            testing_data = np.array([current_cbook.get_BoF_descriptor(get_lbp_data(filename_normal[idx_test]))[0]
-                                     , current_cbook.get_BoF_descriptor(get_lbp_data(filename_dme[idx_test]))[0]])
+            testing_data = np.array([get_lbp_data(filename_normal[idx_test]), get_lbp_data(filename_dme[idx_test])])
 
             # Run the classification for this specific data
             pred_label, roc = Classify(training_data,
@@ -85,7 +84,8 @@ else:
                                        np.array([0, 1], dtype=int),
                                        classifier_str='random-forest',
                                        n_estimators=100,
-                                       n_jobs=20, max_features=None)
+                                       n_jobs=60,
+                                       max_features=None)
 
             results_by_codebook.append((pred_label, roc))
 
