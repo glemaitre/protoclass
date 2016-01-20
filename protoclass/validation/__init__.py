452dbc147ee3790b212e2704982fb986b4839af4
mojdeh <mrastgoo@visor.udg.edu>
2016-01-20 19:10:08 +0100

    Add a function to create automatic table in latex format

--- protoclass/validation/__init__.py
+++ protoclass/validation/__init__.py
@@ -1,11 +1,14 @@
 from .validation import ResultToLabel, ResultToVolume, PlotROCPatients, OpenROCPatients
 
+from .utility import MakeTable
+
 from .metric import LabelsToSensitivitySpecificity, LabelsToPrecisionNegativePredictiveValue, LabelsToGeometricMean, LabelsToAccuracy, LabelsToF1score, LabelsToMatthewCorrCoef, LabelsToGeneralizedIndexBalancedAccuracy
 
 __all__ = ['ResultToLabel', 
            'ResultToVolume', 
            'PlotROCPatients', 
            'OpenROCPatients',
+           'MakeTable',
            'LabelsToSensitivitySpecificity', 
            'LabelsToPrecisionNegativePredictiveValue', 
            'LabelsToGeometricMean', 
