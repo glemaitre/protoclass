746e5738468187304b8abe351553041922b7877d
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-30 18:54:39 +0200

    Commit to have the documentation

---
+++ doc/index.rst
@@ -0,0 +1,21 @@
+.. protoclass documentation master file, created by
+   sphinx-quickstart on Wed Mar 30 18:32:16 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to protoclass's documentation!
+======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- protoclass/preprocessing/__init__.py
+++ protoclass/preprocessing/__init__.py
@@ -4,13 +4,17 @@ pre-processing on medical imaging data.
 """
 
 from .base_normalization import BaseNormalization
+
 from .standalone_normalization import StandaloneNormalization
 from .temporal_normalization import TemporalNormalization
 from .multisequence_normalization import MultisequenceNormalization
+
 from .short_path_normalization import ShortPathNormalization
+from .gaussian_normalisation import GaussianNormalization
 
 __all__ = ['BaseNormalization',
            'StandaloneNormalization',
            'TemporalNormalization',
            'MultisequenceNormalization',
-           'ShortPathNormalization']
+           'ShortPathNormalization',
+           'GaussianNormalization']
