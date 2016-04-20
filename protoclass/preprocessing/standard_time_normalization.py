c44e828c60692f4d8f6586fcd53c81d0652b12ea
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-04-20 21:48:37 +0200

    Create the class to normalize the dce series

---
+++ protoclass/preprocessing/standard_time_normalization.py
@@ -0,0 +1,15 @@
+""" Standard time normalization to normalize temporal modality.
+"""
+
+from .temporal_normalization import TemporalNormalization
+
+
+class StandardTimeNormalization(TemporalNormalization):
+    """ Standard normalization to normalize temporal modality.
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+    """
