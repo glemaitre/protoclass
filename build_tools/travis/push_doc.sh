a9ddbf7183375541f6a577ed753533a4f7886b00
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-09-13 12:25:33 +0200

    Update the push doc file

--- build_tools/travis/push_doc.sh
+++ build_tools/travis/push_doc.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cd $TRAVIS_BUILD_DIR
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 # We need to move to the repo where the doc was generated
 cd $TRAVIS_BUILD_DIR/../$DOC_REPO/html
 
@@ -29,16 +41,6 @@ git config --global user.email $EMAIL
 git config --global user.name $USERNAME
 git add -f ./
 git commit -m "Push the doc automatically"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 git push -f origin gh-pages
 if [ $? -ne 0 ]; then
