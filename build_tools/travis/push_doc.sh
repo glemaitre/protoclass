c00a9d5c818daface815b2153378aa9eb23093a0
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-09-13 15:17:47 +0200

    Still there

--- build_tools/travis/push_doc.sh
+++ build_tools/travis/push_doc.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-cd $TRAVIS_BUILD_DIR
+set -o errexit
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+cd $TRAVIS_BUILD_DIR
 
 # We need to move to the repo where the doc was generated
 cd $TRAVIS_BUILD_DIR/../$DOC_REPO/html
@@ -26,7 +18,7 @@ mkdir tmp
 cd tmp
 
 # Clone the repository to push the doc later
-git clone "git@github.com:$USERNAME/"$PROJECT".git";
+git clone "https://${GITHUB_TOKEN}@$github.com/${PROJECT}.git"
 
 cd $PROJECT
 git branch gh-pages
@@ -42,9 +34,4 @@ git config --global user.name $USERNAME
 git add -f ./
 git commit -m "Push the doc automatically"
 
-git push -f origin gh-pages
-if [ $? -ne 0 ]; then
-    echo "Pushing docs failed"
-    echo
-    exit 1
-fi
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
